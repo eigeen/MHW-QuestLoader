92720239d9f96f935151091c73f8ac06d3e12b53
Eigeen <dengyk2002@qq.com>
2024-03-20 20:00:40 +0800

    LoaderFFI project

---
+++ LoaderFFI/dllmain.cpp
@@ -0,0 +1,25 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include "loader.h"
+
+using namespace loader;
+
+extern "C" void Log(int level, char* message) {
+	LogLevel l;
+	if (level == 0) {
+		l = LogLevel::DEBUG;
+	}
+	else if (level == 1) {
+		l = LogLevel::INFO;
+	}
+	else if (level == 2) {
+		l = LogLevel::WARN;
+	}
+	else if (level == 3) {
+		l = LogLevel::ERR;
+	}
+	else {
+		l = LogLevel::INFO;
+	}
+	LOG(l) << message;
+}
